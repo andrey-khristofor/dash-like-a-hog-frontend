c8a8a822451de2b2e96e9a6c2ff280e34d791ac2
anton.chernov <anton.chernov@wolterskluwer.com>
2021-09-14 18:07:33 +0300

    Removed https

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
 const express = require('express');
 const app = express();
-app.use(requireHTTPS);
 app.use(express.static('./dist/dash-like-a-hog-frontend'));
 
 app.get('/*', function(req, res) {
